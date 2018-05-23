7ebf95face607533c0a6eb5aa89736531ac5a447
didier <didier@ici_ou_labas.fr>
2018-05-23 11:20:36 +0200

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOriginTpJpa https://didier-tp:pwd007!@github.com/didier-tp/m2idef_tpJpa.git
+git remote add gitHubOriginTpJpa https://didier-tp:password@github.com/didier-tp/m2idef_tpJpa.git
 git push -u gitHubOriginTpJpa master
 pause
 

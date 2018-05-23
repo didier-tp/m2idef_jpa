fe420f8343208a263d41cc1adbefa9fae37727f6
didier <didier@ici_ou_labas.fr>
2018-05-23 15:42:02 +0200

    nouvelle version

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,4 @@
-//import.sql est un script SQL automatiquement déclenché au démarrage 
-//par hibernate 
-//de appli (ou test) en mode hibernate.hbm2ddl.auto = create  
+//import.sql est un script SQL automatiquement déclenché par hibernate 
+// au démarrage de appli (ou test) en mode hibernate.hbm2ddl.auto=create  
 INSERT INTO Devise(codeDevise,monnaie,dChange) VALUES ('USD','dollar',1.0)
 INSERT INTO Devise(codeDevise,monnaie,dChange) VALUES ('GBP','livre',0.9)

7346cdc38cb9354ff4d5d1c49b537103e76c7017
didier <didier@ici_ou_labas.fr>
2018-05-23 11:23:15 +0200

    version initiale

--- init_push.bat
+++ init_push.bat
@@ -2,8 +2,8 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOriginJpa https://didier-tp:pwd007@github.com/didier-tp/m2idef_jpa.git
-git push -u gitHubOriginJpa master
+git remote add gitHubOriginJpa2 https://didier-tp:pwd007!@github.com/didier-tp/m2idef_jpa.git
+git push -u gitHubOriginJpa2 master
 pause
 
 REM open with text editor

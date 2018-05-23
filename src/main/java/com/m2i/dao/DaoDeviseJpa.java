056f098f26920dfed4cb4462bce26d64be05fc11
didier <didier@ici_ou_labas.fr>
2018-05-23 11:51:00 +0200

    nouvelle version

--- src/main/java/com/m2i/dao/DaoDeviseJpa.java
+++ src/main/java/com/m2i/dao/DaoDeviseJpa.java
@@ -3,16 +3,22 @@ package com.m2i.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
 
 import com.m2i.entity.Devise;
 
 public class DaoDeviseJpa implements IDaoDevise {
-	
+	//@PersistenceContext (avec Spring ou EJB3)
 	private EntityManager entityManager;
+	
+    //pour version sans spring
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public Devise findDeviseByCode(String code) {
-		return null;
+		return entityManager.find(Devise.class, code /*id/pk*/);
 	}
 
 	@Override
@@ -22,14 +28,39 @@ public class DaoDeviseJpa implements IDaoDevise {
 
 	@Override
 	public void insertDevise(Devise d) {
+		try {
+			entityManager.getTransaction().begin();
+				entityManager.persist(d); //insert into ...
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void updateDevise(Devise d) {
+		try {
+			entityManager.getTransaction().begin();
+				entityManager.merge(d); //insert into ...
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void deleteDevise(String code) {
+		try {
+			entityManager.getTransaction().begin();
+				Devise d = entityManager.find(Devise.class, code);
+				entityManager.remove(d);
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+		}
 	}
 
 }

74701361bcff0175918db5eaf3f719a221808300
didier <didier@ici_ou_labas.fr>
2018-05-24 13:35:23 +0200

    nouvelle version

--- src/test/java/com/m2i/test/TestDaoClient.java
+++ src/test/java/com/m2i/test/TestDaoClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,40 @@ public class TestDaoClient {
 		for(Client cli : listeClient){
 			System.out.println("\t"+cli);
 		}
+	}
+	
+	/*
+	 Remarque importante: 
+	 ===================
+	 dès que le "entityManager" est fermé (ce qui est automatiquement déclenché par Spring
+	 au sein des méthodes marquées via @Transactional dans les services et/ou dao )
+	 tous les objets persistants passent à l'état détachés.
+	 ==> coté Test et coté Web , tous les objets sont détachés de JPA/Hibernate !!!
+	 */
+	
+	@Test
+	public void testCRUD(){
+		//1a.ajout
+		Client c = new Client();
+		c.setPrenom("olie"); c.setNom("Condor"); 
+		dao.insertClient(c);
+		Long numero = c.getNumero();
+		//1b. verif ajout via relecture
+		Client cRelu = dao.findClientByNumero(numero);
+		System.out.println(cRelu.toString()); 
+		Assert.assertTrue(cRelu.getPrenom().equals("olie"));
+		//2b. update
+		c.setPrenom("**nouveauPrenom**");
+		dao.updateClient(c);
+		//2c. verif mise à jour via relecture
+		cRelu = dao.findClientByNumero(numero);
+		Assert.assertTrue(cRelu.getPrenom().equals("**nouveauPrenom**"));
+		System.out.println(cRelu.toString()); 
+		//3a. suppression
+		dao.deleteClient(numero);
+		//3b. verif suppression
+		cRelu=dao.findClientByNumero(numero);
+		Assert.assertTrue(cRelu==null);
+		System.out.println("cRelu normalement null="+cRelu);
 	}
 }
--- src/test/java/com/m2i/test/TestDaoDevise.java
+++ src/test/java/com/m2i/test/TestDaoDevise.java
@@ -34,4 +34,6 @@ public class TestDaoDevise {
 			System.out.println("\t"+dev);
 		}
 	}
+	
+	
 }
